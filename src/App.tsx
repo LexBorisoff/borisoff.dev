7def554289fb3b1e717fb9ec89c7486bbf9e7c9c
Lex Borisoff <lex@borisoff.dev>
2025-02-06 19:25:59 -0500

    style: updated styles and favicon

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { styled } from 'styled-components';
 const Container = styled.div`
   height: 100%;
   width: 100%;
-  background: #141414;
   color: white;
   display: flex;
   justify-content: center;
