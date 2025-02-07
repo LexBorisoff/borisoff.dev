56b0c674ff0feffd2dc23cd38e39c7bf67c84f8f
Lex Borisoff <lex@borisoff.dev>
2025-02-06 23:09:19 -0500

    feat: enhanced hero component with dynamic background and cursor effects

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,13 +4,14 @@ const HeaderContainer = styled.div`
   height: 5rem;
   display: flex;
   justify-content: center;
+  background-color: #0f0f0f96;
 `;
 
 const Wrapper = styled.div`
   height: 100%;
+  max-width: 1200px;
   flex: 1;
   margin: 0 5rem;
-  border-bottom: 1px solid #1d1d1d;
   display: flex;
   justify-content: space-between;
   align-items: center;
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,12 +1,18 @@
+import { useEffect, useRef, useState } from 'react';
 import { styled } from 'styled-components';
 
 import Header from '../header/header';
 
-const HeroContainer = styled.div`
+interface HeroContainerProps {
+  coords: { x: number; y: number };
+}
+const HeroContainer = styled.div<HeroContainerProps>`
   height: 40rem;
   display: flex;
   flex-direction: column;
   justify-content: top;
+  background: ${({ coords: { x, y } }) =>
+    `radial-gradient(40rem 40rem at ${x}% ${y}%, #00115e, transparent)`};
 `;
 
 const HeroContent = styled.div`
@@ -18,11 +24,16 @@ const HeroContent = styled.div`
   align-items: center;
   gap: 3rem;
   padding-bottom: 2rem;
+
+  cursor:
+    url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><text y="13" font-size="13">✨</text></svg>')
+      10 10,
+    auto;
 `;
 
 const TextWrapper = styled.div`
   text-align: center;
-  max-width: 45rem;
+  max-width: 40rem;
   display: flex;
   flex-direction: column;
   gap: 1.5rem;
@@ -35,26 +46,43 @@ const TextWrapper = styled.div`
   }
 
   .hero-main-text {
-    font-size: 4rem;
-    font-weight: 600;
+    font-size: 4.5rem;
+    font-weight: 500;
     line-height: 5rem;
     background-clip: text;
-    background: linear-gradient(69deg, #bbbbbb 10%, #afafaf 40%, #b43058 100%);
+    background: linear-gradient(180deg, #bbbbbb 10%, #afafaf 40%, #797979 100%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
   }
 `;
 
 export default function Hero(): React.ReactNode {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [coords, setCoords] = useState({ x: 100, y: -10 });
+
+  useEffect(() => {
+    if (containerRef.current != null) {
+      containerRef.current.addEventListener('mousemove', (e) => {
+        const x = (e.clientX / window.innerWidth) * 100;
+        const y = (e.clientY / window.innerHeight) * 100;
+        setCoords({ x, y });
+      });
+    }
+  }, []);
+
   return (
-    <HeroContainer className="hero-container">
+    <HeroContainer
+      className="hero-container"
+      ref={containerRef}
+      coords={coords}
+    >
       <Header />
 
       <HeroContent className="hero-content">
         <TextWrapper>
           <span className="hero-intro-text">{"Hi, I'm Lex 👨‍💻"}</span>
           <span className="hero-main-text">
-            I build modern, scalable, and efficient web applications
+            I create magical web experiences
           </span>
         </TextWrapper>
       </HeroContent>
