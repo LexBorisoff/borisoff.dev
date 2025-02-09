0f5cfa98fc1d7cd039345a03c7d3f631959df857
Lex Borisoff <lex@borisoff.dev>
2025-02-09 14:11:12 -0500

    feat: using env variable to display under construction banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -3,7 +3,7 @@ import { styled } from 'styled-components';
 const BannerSection = styled.section`
   padding: 1rem 2rem;
   background: #d98e01;
-  color: #6b3e00;
+  color: #502e00;
   text-align: center;
   font-weight: 600;
 `;
